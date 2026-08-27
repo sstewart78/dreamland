57c036b23b8b0d8527c1f50a3ed9a1435da93e79
sstewart78 <samuel.stewart57@gmail.com>
2026-08-27 10:39:01 -0700

    Remove extraneous lines

--- src/Vignette/Vignette.tsx
+++ src/Vignette/Vignette.tsx
@@ -1,5 +1,3 @@
-
-
 export function Vignette ({bgOpacity}: {bgOpacity: number}) {
     return (
         <div className='absolute inset-0 bg-linear-to-t from-[#030508] via-transparent to-black/60 transition-opacity duration-500'
