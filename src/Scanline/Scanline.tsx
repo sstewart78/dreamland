3fb75063c76aa0b23dcac9efeac87197b595c10d
sstewart78 <samuel.stewart57@gmail.com>
2026-08-27 12:31:19 -0700

    New component Scanline

---
+++ src/Scanline/Scanline.tsx
@@ -0,0 +1,7 @@
+export function Scanline() {
+    return (
+        <div className="absolute inset-0 pointer-events-none z-30 opacity-15 bg-[linear-gradient(rgba(18,16,16,0)_50%,rgba(0,0,0,0.4)_50%)] bg-size-[100%_4px]" />
+    );
+}
+
+export default Scanline;
